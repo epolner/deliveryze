4b6306033ea0dbcc2f20b0fe732276e06ac2d92c
Alex Kolchinski <y.a.kolchinski@gmail.com>
2014-02-03 19:55:56 -0600

    Continued factoring out local settings

--- deliveryze/settings.py
+++ deliveryze/settings.py
@@ -1,5 +1,6 @@
 # Django settings for deliveryze project.
 import os
+import settings_local
 
 PROJECT_PATH = os.path.realpath(os.path.join(os.path.dirname(__file__), '..'))
 
@@ -11,18 +12,6 @@ ADMINS = (
 )
 
 MANAGERS = ADMINS
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'delivsite',                      # Or path to database file if using sqlite3.
-        # The following settings are not used with sqlite3:
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'localhost',                      # Empty for localhost through domain sockets or '128.0.0.1' for localhost through TCP.
-        'PORT': '',                      # Set to empty string for default.
-    }
-}
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
