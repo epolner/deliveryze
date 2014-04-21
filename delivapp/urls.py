bf87512b45b180e4fcd1e273160c08b2bfbbec00
Alex Kolchinski <y.a.kolchinski@gmail.com>
2014-04-21 16:34:29 -0500

    Changed front end to use template from preregistration site - work in progress!

--- delivapp/urls.py
+++ delivapp/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, url
 
 from delivapp import views
+from deliveryze import settings
 
 urlpatterns = patterns('',
   url(r'^$', views.homepage, name='homepage'),
@@ -11,3 +12,10 @@ urlpatterns = patterns('',
   url(r'^placeorder$', views.placeorder, name='placeorder'),
   url(r'^processing$', views.processing, name='processing'),
 )
+
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+urlpatterns += patterns('', (
+    r'^static/(?P<path>.*)$',
+    'django.views.static.serve',
+    {'document_root': settings.STATIC_ROOT}
+))
--- deliveryze/settings.py
+++ deliveryze/settings.py
@@ -59,12 +59,14 @@ STATIC_ROOT = ''
 # Example: "http://example.com/static/", "http://static.example.com/"
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = ( os.path.join(PROJECT_PATH,'static/'),)
+
 # Additional locations of static files
-STATICFILES_DIRS = (
+#STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-)
+#)
 
 # List of finder classes that know how to find static files in
 # various locations.
